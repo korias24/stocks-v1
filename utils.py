19b52596ec405496d93ec86ab5f5dde78e7a3e0b
Enis <enis.k.inan@gmail.com>
2021-03-01 22:25:20 -0800

    Update fractional filter with absolutely and temporarily ignored stuff
    Signed-off-by: Enis <enis.k.inan@gmail.com>

--- utils.py
+++ utils.py
@@ -23,22 +23,29 @@ def write_tickers(path, tickers):
     with open(path, 'w') as f:
         f.write("\n".join([t.symbol for _, t in tickers.items()]))
 
-td = datetime.date(2021, 2, 26)
+td = datetime.date(2021, 3, 1)
 dp = EODHD(api_token = os.environ.get('EODHD_API_TOKEN'), cache_dir = '/Users/enis.inan/.stocks_cache')
 tickers = dp.tickers('US')
 
 # dfilter => default filter
 dfilter = andf(
     ignore_symbols(*parse_ignored_companies()),
+    ignore_symbols(*parse_ignored_companies('/Users/enis.inan/GitHub/stocks/absolutely_ignored_companies.txt')),
+    ignore_symbols(*parse_ignored_companies('/Users/enis.inan/GitHub/stocks/temporarily_ignored_companies.txt')),
     ignore_exchanges('OTCGREY'),
-    close(td, andp(gt(0), lt(0.05))),
+    close(td, andp(gte(0.01), lte(0.022))),
     market_cap(td, gte(3000000)),
-    min_volume(1000000, td - datetime.timedelta(days = 14), td, 1)
+    min_volume(10000000, td - datetime.timedelta(days = 14), td, 3)
 )
 
 sfilter = andf(
+    ignore_symbols(*parse_ignored_companies('/Users/enis.inan/GitHub/stocks/absolutely_ignored_companies.txt')),
+    ignore_symbols(*parse_ignored_companies('/Users/enis.inan/GitHub/stocks/temporarily_ignored_companies.txt')),
     ignore_exchanges('OTCGREY'),
     close(td, andp(gt(0), lt(0.005))),
     market_cap(td, gte(3000000)),
     min_volume(2000000, td - datetime.timedelta(days = 14), td, 7)
 )
+
+print("REMINDER: DELETE TEMPORARILY IGNORED COMPANIES")
+
